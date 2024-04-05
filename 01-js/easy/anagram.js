477949b11ed218d3dd8c75518e18b49903dfed7f
shams alam <103259015+shams220@users.noreply.github.com>
2024-04-05 12:09:32 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,28 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+        return false;
+    }
 
+    let isAnagram = true;
+
+    for (let i = 0; i < str1.length; i++) {
+        let isFound = false;
+        for (let j = 0; j < str1.length; j++) {
+            if (str1.charAt(i) === str2.charAt(j)) {
+                isFound = true;
+                break;
+            }
+        }
+        if (!isFound) {
+            isAnagram = false;
+            break;
+        }
+    }
+
+    return isAnagram;
+}
 }
 
 module.exports = isAnagram;
