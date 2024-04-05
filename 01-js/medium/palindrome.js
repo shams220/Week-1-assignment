275931e1c391d2b3da6be7beb8bd07be114913e5
shams alam <103259015+shams220@users.noreply.github.com>
2024-04-05 12:22:14 +0530

    Update palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,6 +7,11 @@
 */
 
 function isPalindrome(str) {
+  for(let i = 0;i<str.length/2;i++){
+    if(str.charAt(i) !== str.charAt(str.length-1-i)){
+        return false;
+    }
+}
   return true;
 }
 
