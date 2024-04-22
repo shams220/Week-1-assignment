9c31841a2cebe39f0853d1477478e46453403778
shams alam <103259015+shams220@users.noreply.github.com>
2024-04-22 21:09:52 +0530

    Update 1-promisify-setTimeout.js

--- 02-async-js/hard (promises)/1-promisify-setTimeout.js
+++ 02-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -2,5 +2,11 @@
     Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
 */
 
-function wait(n) {
+function wait(n){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("i am a programmer");
+            reject("can not be fetched");
+        },n*1000)
+    })
 }
