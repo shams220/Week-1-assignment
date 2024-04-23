266c81babeeca570a8702a4e3087b608efd00402
shams alam <103259015+shams220@users.noreply.github.com>
2024-04-23 18:06:11 +0530

    Update 2-sleep-completely.js

--- 02-async-js/hard (promises)/2-sleep-completely.js
+++ 02-async-js/hard (promises)/2-sleep-completely.js
@@ -3,6 +3,16 @@
  * During this time the thread should not be able to do anything else.
  */
 
-function sleep (seconds) {
-
+function sleep(seconds){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            
+            resolve("this is functions is made to sleep all tasks until it is executed");
+        },seconds*1000)
+    })
 }
+async function api(){
+    await sleep(4);
+    console.log("yes it waited");
+}
+api()
